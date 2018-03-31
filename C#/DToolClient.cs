0552f88808cb33f6e3d93d5fd53b29b79bcd81d1
zd304 <568892758@qq.com>
2018-03-31 11:16:45 +0800

    2

---
+++ C#/DToolClient.cs
@@ -0,0 +1,159 @@
+﻿using UnityEngine;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum DTool_STC
+{
+	DTool_STC_None					= 0,
+	DTool_STC_ReqObject				= 1,
+	DTool_STC_ReqActive				= 2,
+
+	DTool_STC_Count
+}
+
+public enum DTool_CTS
+{
+	DTool_CTS_None					= 0,
+	DTool_CTS_UpdateHierarchy		= 1,
+	DTool_CTS_UpdateObject			= 2,
+	DTool_CTS_AddLog				= 3,
+
+	DTool_CTS_Count
+}
+
+public class DToolClient : MonoBehaviour
+{
+	void Start()
+	{
+		InitServer();
+	}
+
+	void InitServer()
+	{
+		IPAddress ipAdr = IPAddress.Parse("127.0.0.1");
+		ipep = new IPEndPoint(ipAdr, 5304);
+
+		clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+		clientSocket.Connect(ipep);
+
+		thread = new Thread(new ThreadStart(GoClient));
+		thread.Start();
+
+		logic = new DToolLogic(this);
+		logic.Init();
+		StartCoroutine(DelayInit());
+	}
+
+	IEnumerator DelayInit()
+	{
+		yield return new WaitForEndOfFrame();
+
+		logic.UpdateHierarchy();
+	}
+
+	void GoClient()
+	{
+		while (true)
+		{
+			if (clientSocket == null)
+				return;
+			byte[] receiveData = new byte[1024];
+			int recv = clientSocket.Receive(receiveData);
+			if (recv == 0)
+			{
+				Connect();
+				continue;
+			}
+
+			lock (this)
+			{
+				mRecvData = receiveData;
+				mRecvLen = recv;
+			}
+
+			//Debug.LogError(msg);
+		}
+	}
+
+	void Connect()
+	{
+		if (clientSocket != null)
+		{
+			//clientSocket.Shutdown(SocketShutdown.Both);
+			clientSocket.Close();
+			clientSocket = null;
+			return;
+		}
+		clientSocket.Connect(ipep);
+	}
+
+	public void SendToServer(int ctsType, string msg)
+	{
+		if (clientSocket == null)
+			return;
+
+		byte[] intBuff = System.BitConverter.GetBytes(ctsType);
+		byte[] concent = Encoding.UTF8.GetBytes(msg);
+		byte[] tmp = new byte[intBuff.Length + concent.Length];
+		System.Buffer.BlockCopy(intBuff, 0, tmp, 0, intBuff.Length);
+		System.Buffer.BlockCopy(concent, 0, tmp, intBuff.Length, concent.Length);
+		try
+		{
+			int count = clientSocket.Send(tmp, tmp.Length, SocketFlags.None);
+		}
+		catch (SocketException e)
+		{
+			Debug.LogError(e.Message);
+		}
+	}
+
+	void OnEnable()
+	{
+		Application.logMessageReceived += HandleLog;
+	}
+
+	void OnDisable()
+	{
+		clientSocket.Shutdown(SocketShutdown.Both);
+		clientSocket.Close();
+
+		if (thread != null)
+		{
+			thread.Interrupt();
+			thread.Abort();
+		}
+	}
+
+	void Update()
+	{
+		lock (this)
+		{
+			if (mRecvData != null && mRecvLen > 0)
+			{
+				int stc = System.BitConverter.ToInt32(mRecvData, 0);
+				string msg = Encoding.UTF8.GetString(mRecvData, 4, mRecvLen - 4);
+
+				logic.OnMsg((DTool_STC)stc, msg);
+
+				mRecvData = null;
+				mRecvLen = 0;
+			}
+		}
+	}
+
+	void HandleLog(string message, string stack, LogType type)
+	{
+		logic.AddLog(message, stack, type);
+	}
+
+	Thread thread;
+	Socket clientSocket = null;
+	IPEndPoint ipep;
+	public DToolLogic logic = null;
+	byte[] mRecvData = null;
+	int mRecvLen = 0;
+}
---
+++ C#/DToolLogic.cs
@@ -0,0 +1,158 @@
+﻿using UnityEngine;
+using LitJson;
+using System;
+using System.Text;
+using System.Collections.Generic;
+
+public class DToolLogic
+{
+	public DToolLogic(DToolClient client)
+	{
+		mClient = client;
+	}
+
+	public void UpdateHierarchy()
+	{
+		JsonData root = new JsonData();
+		GameObject[] gos = GameObject.FindObjectsOfType<GameObject>();//UnityEngine.Profiling.Profiler;
+		for (int i = 0; i < gos.Length; ++i)
+		{
+			GameObject go = gos[i];
+			if (go.transform.parent != null)
+				continue;
+			string nameInfo = "";
+			StringBuilder sb = new StringBuilder(nameInfo);
+			int active = 0;
+			if (go.activeSelf)
+				active = 1;
+			sb.Append(go.name).Append ("+").Append(active);
+			JsonData node = new JsonData();
+			node["n"] = go.name;
+			node["a"] = active;
+			JsonData chidren = new JsonData();
+			node["c"] = chidren;
+			root.Add(node);
+			AddChildNode(go, ref chidren);
+		}
+
+		string msg = root.ToJson();
+		msg = msg.Replace("\"c\":}", "\"c\":[]}");
+		mClient.SendToServer((int)DTool_CTS.DTool_CTS_UpdateHierarchy, msg);
+	}
+
+	void AddChildNode(GameObject rootGO, ref JsonData jsonRoot)
+	{
+		List<GameObject> children = new List<GameObject>();
+		for (int i = 0; i < rootGO.transform.childCount; ++i)
+		{
+			Transform tChild = rootGO.transform.GetChild(i);
+			GameObject go = tChild.gameObject;
+
+			string nameInfo = "";
+			StringBuilder sb = new StringBuilder(nameInfo);
+			int active = 0;
+			if (go.activeSelf)
+				active = 1;
+			sb.Append(go.name).Append ("+").Append(active);
+			JsonData node = new JsonData();
+			node["n"] = go.name;
+			node["a"] = active;
+			JsonData chidren = new JsonData();
+			node["c"] = chidren;
+			jsonRoot.Add(node);
+			AddChildNode(go, ref chidren);
+		}
+	}
+
+	public void AddLog(string message, string stack, LogType type)
+	{
+		JsonData node = new JsonData();
+		node["m"] = message;
+		node["s"] = stack;
+		node["t"] = (int)type;
+		string msg = node.ToJson();
+		mClient.SendToServer((int)DTool_CTS.DTool_CTS_AddLog, msg);
+	}
+
+	public void OnMsg(DTool_STC stcType, string msg)
+	{
+		cbMsg cb = mCBMsg[(int)stcType];
+		if (cb != null)
+		{
+			JsonData json = JsonMapper.ToObject<JsonData>(msg);
+			cb(json, mClient);
+		}
+	}
+
+	static GameObject FindObject(string path)
+	{
+		GameObject go = GameObject.Find(path);
+		if (go == null)
+		{
+			int index = path.IndexOf('/');
+			if (index <= -1)
+				return null;
+			string headPath = path.Substring(0, index);
+			GameObject root = GameObject.Find(headPath);
+			if (root == null)
+				return null;
+			path = path.Substring(index + 1);
+			Transform t = root.transform.Find(path);
+			if (t == null)
+				return null;
+			go = t.gameObject;
+		}
+		return go;
+	}
+
+	////////////////////////////////////////////////////////////RegisterCallback////////////////////////////////////////////////////////////////////////////////////
+
+	static void Msg_ReqObject(JsonData json, DToolClient client)
+	{
+		string path = json["p"].ToString();
+		GameObject go = FindObject(path);
+		if (go == null)
+			return;
+		int active = 0;
+		if (go.activeSelf)
+			active = 1;
+
+		JsonData jsonData = new JsonData();
+		jsonData["p"] = path;
+		jsonData["a"] = active;
+		jsonData["px"] = go.transform.position.x;
+		jsonData["py"] = go.transform.position.y;
+		jsonData["pz"] = go.transform.position.z;
+		jsonData["rx"] = go.transform.eulerAngles.x;
+		jsonData["ry"] = go.transform.eulerAngles.y;
+		jsonData["rz"] = go.transform.eulerAngles.z;
+		jsonData["sx"] = go.transform.localScale.x;
+		jsonData["sy"] = go.transform.localScale.y;
+		jsonData["sz"] = go.transform.localScale.z;
+		client.SendToServer((int)DTool_CTS.DTool_CTS_UpdateObject, jsonData.ToJson());
+	}
+
+	static void Msg_ReqActive(JsonData json, DToolClient client)
+	{
+		string path = json["p"].ToString();
+		int active = 0;
+		if (!int.TryParse(json["a"].ToString(), out active))
+		{
+			return;
+		}
+		GameObject go = FindObject(path);
+		if (go == null)
+			return;
+		go.SetActive(active > 0);
+	}
+
+	public void Init()
+	{
+		mCBMsg[(int)DTool_STC.DTool_STC_ReqObject] = Msg_ReqObject;
+		mCBMsg[(int)DTool_STC.DTool_STC_ReqActive] = Msg_ReqActive;
+	}
+
+	DToolClient mClient;
+	public delegate void cbMsg(JsonData json, DToolClient client);
+	public cbMsg[] mCBMsg = new cbMsg[(int)DTool_STC.DTool_STC_Count];
+}
